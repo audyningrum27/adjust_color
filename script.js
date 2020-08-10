9ead9d158a30421f55712f4df3b4712a652c9d2f
audyningrum27 <66819730+audyningrum27@users.noreply.github.com>
2020-08-10 17:04:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const input= document.getElementById('colorCode');
+const liveCode = document.getElementById('liveCode');
+
+// const warna = () => {
+//     button.style.borderColor = "black";
+// }
+
+const getColor = () => {
+    return input.value;
+}
+
+const AdjustColor = () => {
+    body.style.backgroundColor = `${getColor()}`;
+}
+
+const getNewColor = () => {
+    let symbols, color;
+    symbols = "0123456789ABCDEF"
+    color = "#"
+    for(let i = 0; i < 6; i++) {
+        color += symbols[Math.floor(Math.random() * 16)];
+    }
+    button.style.backgroundColor = color;
+    liveCode.innerHTML = color;
+}
+
+button.addEventListener('click', () => {
+    AdjustColor();
+    getNewColor();
+    warna();
+})
